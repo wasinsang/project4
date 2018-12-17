e6d1bcf3a024043cb79a94d79ca08854bccaf1f6
wasin <wasinsang@gmail.com>
2018-12-18 00:00:22 +0700

    My Comment to Add File

--- lesson_7_robot_trackline.ino
+++ lesson_7_robot_trackline.ino
@@ -1,18 +1,18 @@
 int ssr = 12;
 int ssl = 13;
-int M1_R = 11;
-int M2_R = 10;
-int pwm_R = 9;
-int M1_L = 8;
-int M2_L = 7;
-int pwm_L = 6;
+int MA_R = 6;
+int MB_R = 5;
+int pwm_R = 7;
+int MA_L = 4;
+int MB_L = 3;
+int pwm_L = 2;
 void setup() {
   pinMode (ssr, INPUT);
   pinMode (ssl, INPUT);
-  pinMode (M1_R, OUTPUT);
-  pinMode (M2_R, OUTPUT);
-  pinMode (M1_L, OUTPUT);
-  pinMode (M2_L, OUTPUT);
+  pinMode (MA_R, OUTPUT);
+  pinMode (MB_R, OUTPUT);
+  pinMode (MA_L, OUTPUT);
+  pinMode (MB_L, OUTPUT);
   pinMode (pwm_R, OUTPUT);
   pinMode (pwm_L, OUTPUT);
 }
@@ -24,26 +24,26 @@ void loop() {
   sl = digitalRead (ssl);
   if (sr == 0)
   {
-    digitalWrite (M1_R, 1);
-    digitalWrite (M2_R, 0);
-    analogWrite (pwm_R, 255);
+    digitalWrite (MA_R, 1);
+    digitalWrite (MB_R, 0);
+    analogWrite (pwm_R, 0);
   }
   if (sl == 0)
   {
-    digitalWrite (M1_L, 1);
-    digitalWrite (M2_L, 0);
-    analogWrite (pwm_L, 255);
+    digitalWrite (MA_L, 1);
+    digitalWrite (MB_L, 0);
+    analogWrite (pwm_L, 0);
   }
   if (sl == 1)
   {
-    digitalWrite (M1_L, 1);
-    digitalWrite (M2_L, 0);
-    analogWrite (pwm_L, 55);
+    digitalWrite (MA_L, 1);
+    digitalWrite (MB_L, 0);
+    analogWrite (pwm_L, 255);
   }
   if (sr == 1)
   {
-    digitalWrite (M1_R, 1);
-    digitalWrite (M2_R, 0);
-    analogWrite (pwm_R, 55);
+    digitalWrite (MA_R, 1);
+    digitalWrite (MB_R, 0);
+    analogWrite (pwm_R, 255);
   }
 }
--- lesson_8_altrasonic_robot.ino
+++ lesson_8_altrasonic_robot.ino
@@ -2,16 +2,12 @@
 
 Ultrasonic ultrasonic(4, 5); // 4 tric 5 echo
 
-int Motor_R1 = 13;
-int Motor_R2 = 12;
-int Motor_L1 = 10;
-int Motor_L2 = 9;
-int pwmr = 11;
-int pwml = 8;
-int ssR = 7;
-int ssL = 6;
-int swl = 5;
-int swr = 4;
+int Motor_R1 = 6;
+int Motor_R2 = 5;
+int Motor_L1 = 4;
+int Motor_L2 = 3;
+int pwmr = 7;
+int pwml = 2;
 
 int distance;
 
@@ -22,10 +18,6 @@ void setup() {
   pinMode (Motor_L2, OUTPUT);
   pinMode (pwmr, OUTPUT);
   pinMode (pwml, OUTPUT);
-  pinMode (ssR, INPUT);
-  pinMode (ssL, INPUT);
-  pinMode (swl, INPUT);
-  pinMode (swr, INPUT);
   Serial.begin (9600);
 }
 
@@ -35,12 +27,11 @@ void loop() {
   int l = digitalRead (ssL);
   int swR = digitalRead (swr);
   int swL = digitalRead (swl);
-
-
+  Serial.println(distance);
   if (distance <= 7)
   {
-    delay(1000)
-    if (distacne <= 7 )
+    delay(1000);
+    if (distance <= 7 )
     {
       Move_r();
       delay(500);
@@ -53,31 +44,6 @@ void loop() {
   }
 }
 
-
-void right_foword();
-{
-  digitalWrite (Motor_R1, 1);
-  digitalWrite (Motor_R2, 0);
-  analogWrite (pwmr, 150);
-}
-void left_foword();
-{
-  digitalWrite (Motor_L1, 1);
-  digitalWrite (Motor_L2, 0);
-  analogWrite (pwml, 1500);
-}
-void right_slow();
-{
-  digitalWrite (Motor_R1, 1);
-  digitalWrite (Motor_R2, 0);
-  analogWrite (pwmr, 50);
-}
-void left_slow();
-{
-  digitalWrite (Motor_L1, 1);
-  digitalWrite (Motor_L2, 0);
-  analogWrite (pwml, 50);
-}
 void Move_r()
 {
   digitalWrite (Motor_R1, 1);
@@ -92,8 +58,8 @@ void Move_f()
 {
   digitalWrite (Motor_R1, 1);
   digitalWrite (Motor_R2, 0);
-  analogWrite  (pwmr, 50);
+  analogWrite  (pwmr, 250);
   digitalWrite (Motor_L1, 1);
   digitalWrite (Motor_L2, 0);
-  analogWrite  (pwml, 50);
+  analogWrite  (pwml, 250);
 }
